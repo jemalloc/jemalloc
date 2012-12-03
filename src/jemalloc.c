6e6164ae159d9c3bd4f44bd2cba6fc3237687c80
Garrett Cooper <yanegomi@gmail.com>
2012-12-02 17:56:25 -0800

    Don't mangle errno with free(3) if utrace(2) fails
    This ensures POLA on FreeBSD (at least) as free(3) is generally assumed
    to not fiddle around with errno.
    
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- src/jemalloc.c
+++ src/jemalloc.c
@@ -83,11 +83,13 @@ typedef struct {
 #ifdef JEMALLOC_UTRACE
 #  define UTRACE(a, b, c) do {						\
 	if (opt_utrace) {						\
+		int utrace_serrno = errno;				\
 		malloc_utrace_t ut;					\
 		ut.p = (a);						\
 		ut.s = (b);						\
 		ut.r = (c);						\
 		utrace(&ut, sizeof(ut));				\
+		errno = utrace_serrno;					\
 	}								\
 } while (0)
 #else
