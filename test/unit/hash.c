eca367b77909ebd649fbd0430e1e9b80dded14e0
Jason Evans <jasone@canonware.com>
2013-12-17 09:14:39 -0800

    Fix a typo in a string constant.

--- test/unit/hash.c
+++ test/unit/hash.c
@@ -54,7 +54,7 @@ hash_variant_string(hash_variant_t variant)
 	switch (variant) {
 	case hash_variant_x86_32: return ("hash_x86_32");
 	case hash_variant_x86_128: return ("hash_x86_128");
-	case hash_variant_x64_128: return ("hash_64_128");
+	case hash_variant_x64_128: return ("hash_x64_128");
 	default: not_reached();
 	}
 }
