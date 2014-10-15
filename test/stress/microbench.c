acbcbad1e18d3082ee6ce851994ed03f63ae55bd
Jason Evans <jasone@canonware.com>
2014-10-15 14:49:14 -0700

    Thwart compiler optimizations.

--- test/stress/microbench.c
+++ test/stress/microbench.c
@@ -114,6 +114,10 @@ malloc_mus_free(void)
 	void *p;
 
 	p = malloc(1);
+	if (p == NULL) {
+		test_fail("Unexpected malloc() failure");
+		return;
+	}
 	malloc_usable_size(p);
 	free(p);
 }
@@ -124,6 +128,10 @@ malloc_sallocx_free(void)
 	void *p;
 
 	p = malloc(1);
+	if (p == NULL) {
+		test_fail("Unexpected malloc() failure");
+		return;
+	}
 	if (sallocx(p, 0) < 1)
 		test_fail("Unexpected sallocx() failure");
 	free(p);
@@ -143,6 +151,10 @@ malloc_nallocx_free(void)
 	void *p;
 
 	p = malloc(1);
+	if (p == NULL) {
+		test_fail("Unexpected malloc() failure");
+		return;
+	}
 	if (nallocx(1, 0) < 1)
 		test_fail("Unexpected nallocx() failure");
 	free(p);
