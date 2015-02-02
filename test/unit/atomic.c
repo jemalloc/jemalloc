a55dfa4b0af68f372782e130031483ad73cf7eec
Jason Evans <je@fb.com>
2015-02-02 13:49:08 -0800

    Implement more atomic operations.
    - atomic_*_p().
    - atomic_cas_*().
    - atomic_write_*().

--- test/unit/atomic.c
+++ test/unit/atomic.c
@@ -4,48 +4,64 @@
 struct p##_test_s {							\
 	t	accum0;							\
 	t	x;							\
+	t	s;							\
 };									\
 typedef struct p##_test_s p##_test_t;
 
-#define	TEST_BODY(p, t, PRI) do {					\
+#define	TEST_BODY(p, t, tc, ta, PRI) do {				\
 	const p##_test_t tests[] = {					\
-		{-1, -1},						\
-		{-1,  0},						\
-		{-1,  1},						\
+		{(t)-1, (t)-1, (t)-2},					\
+		{(t)-1, (t) 0, (t)-2},					\
+		{(t)-1, (t) 1, (t)-2},					\
 									\
-		{ 0, -1},						\
-		{ 0,  0},						\
-		{ 0,  1},						\
+		{(t) 0, (t)-1, (t)-2},					\
+		{(t) 0, (t) 0, (t)-2},					\
+		{(t) 0, (t) 1, (t)-2},					\
 									\
-		{ 1, -1},						\
-		{ 1,  0},						\
-		{ 1,  1},						\
+		{(t) 1, (t)-1, (t)-2},					\
+		{(t) 1, (t) 0, (t)-2},					\
+		{(t) 1, (t) 1, (t)-2},					\
 									\
-		{0, -(1 << 22)},					\
-		{0, (1 << 22)},						\
-		{(1 << 22), -(1 << 22)},				\
-		{(1 << 22), (1 << 22)}					\
+		{(t)0, (t)-(1 << 22), (t)-2},				\
+		{(t)0, (t)(1 << 22), (t)-2},				\
+		{(t)(1 << 22), (t)-(1 << 22), (t)-2},			\
+		{(t)(1 << 22), (t)(1 << 22), (t)-2}			\
 	};								\
 	unsigned i;							\
 									\
 	for (i = 0; i < sizeof(tests)/sizeof(p##_test_t); i++) {	\
+		bool err;						\
 		t accum = tests[i].accum0;				\
-		assert_u64_eq(atomic_read_##p(&accum), tests[i].accum0,	\
-		    "i=%u", i);						\
-		assert_u64_eq(atomic_add_##p(&accum, tests[i].x),	\
-		    tests[i].accum0 + tests[i].x,			\
-		    "i=%u, accum=%#"PRI", x=%#"PRI,			\
+		assert_##ta##_eq(atomic_read_##p(&accum),		\
+		    tests[i].accum0,					\
+		    "Erroneous read, i=%u", i);				\
+									\
+		assert_##ta##_eq(atomic_add_##p(&accum, tests[i].x),	\
+		    (t)((tc)tests[i].accum0 + (tc)tests[i].x),		\
+		    "i=%u, accum=%"PRI", x=%"PRI,			\
 		    i, tests[i].accum0, tests[i].x);			\
-		assert_u64_eq(atomic_read_##p(&accum), accum,		\
-		    "i=%u", i);						\
+		assert_##ta##_eq(atomic_read_##p(&accum), accum,	\
+		    "Erroneous add, i=%u", i);				\
 									\
 		accum = tests[i].accum0;				\
-		assert_u64_eq(atomic_sub_##p(&accum, tests[i].x),	\
-		    tests[i].accum0 - tests[i].x,			\
-		    "i=%u, accum=%#"PRI", x=%#"PRI,			\
+		assert_##ta##_eq(atomic_sub_##p(&accum, tests[i].x),	\
+		    (t)((tc)tests[i].accum0 - (tc)tests[i].x),		\
+		    "i=%u, accum=%"PRI", x=%"PRI,			\
 		    i, tests[i].accum0, tests[i].x);			\
-		assert_u64_eq(atomic_read_##p(&accum), accum,		\
-		    "i=%u", i);						\
+		assert_##ta##_eq(atomic_read_##p(&accum), accum,	\
+		    "Erroneous sub, i=%u", i);				\
+									\
+		accum = tests[i].accum0;				\
+		err = atomic_cas_##p(&accum, tests[i].x, tests[i].s);	\
+		assert_b_eq(err, tests[i].accum0 != tests[i].x,		\
+		    "Erroneous cas success/failure result");		\
+		assert_##ta##_eq(accum, err ? tests[i].accum0 :		\
+		    tests[i].s, "Erroneous cas effect, i=%u", i);	\
+									\
+		accum = tests[i].accum0;				\
+		atomic_write_##p(&accum, tests[i].s);			\
+		assert_##ta##_eq(accum, tests[i].s,			\
+		    "Erroneous write, i=%u", i);			\
 	}								\
 } while (0)
 
@@ -56,7 +72,7 @@ TEST_BEGIN(test_atomic_uint64)
 #if !(LG_SIZEOF_PTR == 3 || LG_SIZEOF_INT == 3)
 	test_skip("64-bit atomic operations not supported");
 #else
-	TEST_BODY(uint64, uint64_t, PRIx64);
+	TEST_BODY(uint64, uint64_t, uint64_t, u64, PRIx64);
 #endif
 }
 TEST_END
@@ -65,7 +81,15 @@ TEST_STRUCT(uint32, uint32_t)
 TEST_BEGIN(test_atomic_uint32)
 {
 
-	TEST_BODY(uint32, uint32_t, PRIx32);
+	TEST_BODY(uint32, uint32_t, uint32_t, u32, "#"PRIx32);
+}
+TEST_END
+
+TEST_STRUCT(p, void *)
+TEST_BEGIN(test_atomic_p)
+{
+
+	TEST_BODY(p, void *, uintptr_t, ptr, "p");
 }
 TEST_END
 
@@ -73,7 +97,7 @@ TEST_STRUCT(z, size_t)
 TEST_BEGIN(test_atomic_z)
 {
 
-	TEST_BODY(z, size_t, "zx");
+	TEST_BODY(z, size_t, size_t, zu, "#zx");
 }
 TEST_END
 
@@ -81,7 +105,7 @@ TEST_STRUCT(u, unsigned)
 TEST_BEGIN(test_atomic_u)
 {
 
-	TEST_BODY(u, unsigned, "x");
+	TEST_BODY(u, unsigned, unsigned, u, "#x");
 }
 TEST_END
 
@@ -92,6 +116,7 @@ main(void)
 	return (test(
 	    test_atomic_uint64,
 	    test_atomic_uint32,
+	    test_atomic_p,
 	    test_atomic_z,
 	    test_atomic_u));
 }
