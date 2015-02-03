008267b9f6a0e4d92a78f0e8c0697248020fc8d3
Felix Janda <felix.janda@posteo.de>
2015-02-03 18:58:02 +0100

    util.c: strerror_r returns char* only on glibc

--- src/util.c
+++ src/util.c
@@ -84,7 +84,7 @@ buferror(int err, char *buf, size_t buflen)
 	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(), 0,
 	    (LPSTR)buf, buflen, NULL);
 	return (0);
-#elif defined(_GNU_SOURCE)
+#elif defined(__GLIBC__) && defined(_GNU_SOURCE)
 	char *b = strerror_r(err, buf, buflen);
 	if (b != buf) {
 		strncpy(buf, b, buflen);
