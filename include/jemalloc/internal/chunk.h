f044bb219e9bfcc585f64f097e5ab0b5837c0451
Jason Evans <jasone@canonware.com>
2015-03-06 20:05:16 -0800

    Change default chunk size from 4 MiB to 256 KiB.
    Recent changes have improved huge allocation scalability, which removes
    upward pressure to set the chunk size so large that huge allocations are
    rare.  Smaller chunks are more likely to completely drain, so set the
    default to the smallest size that doesn't leave excessive unusable
    trailing space in chunk headers.

--- include/jemalloc/internal/chunk.h
+++ include/jemalloc/internal/chunk.h
@@ -5,7 +5,7 @@
  * Size and alignment of memory chunks that are allocated by the OS's virtual
  * memory system.
  */
-#define	LG_CHUNK_DEFAULT	22
+#define	LG_CHUNK_DEFAULT	18
 
 /* Return the chunk address for allocation address a. */
 #define	CHUNK_ADDR2BASE(a)						\
