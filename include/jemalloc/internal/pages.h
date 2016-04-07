6f718446596c83cadc3fa53625953291655bfcdf
Jason Evans <jasone@canonware.com>
2016-04-07 10:14:37 -0400

    Move *PAGE* definitions to pages.h.

--- include/jemalloc/internal/pages.h
+++ include/jemalloc/internal/pages.h
@@ -1,6 +1,21 @@
 /******************************************************************************/
 #ifdef JEMALLOC_H_TYPES
 
+/* Page size.  LG_PAGE is determined by the configure script. */
+#ifdef PAGE_MASK
+#  undef PAGE_MASK
+#endif
+#define	PAGE		((size_t)(1U << LG_PAGE))
+#define	PAGE_MASK	((size_t)(PAGE - 1))
+
+/* Return the page base address for the page containing address a. */
+#define	PAGE_ADDR2BASE(a)						\
+	((void *)((uintptr_t)(a) & ~PAGE_MASK))
+
+/* Return the smallest pagesize multiple that is >= s. */
+#define	PAGE_CEILING(s)							\
+	(((s) + PAGE_MASK) & ~PAGE_MASK)
+
 #endif /* JEMALLOC_H_TYPES */
 /******************************************************************************/
 #ifdef JEMALLOC_H_STRUCTS
