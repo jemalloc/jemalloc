5f118307543b128e1ad6298ec2ab1acd71140095
Jason Evans <jasone@canonware.com>
2017-02-05 23:57:16 -0800

    Replace spin_init() with SPIN_INITIALIZER.

--- include/jemalloc/internal/spin_inlines.h
+++ include/jemalloc/internal/spin_inlines.h
@@ -2,16 +2,10 @@
 #define JEMALLOC_INTERNAL_SPIN_INLINES_H
 
 #ifndef JEMALLOC_ENABLE_INLINE
-void	spin_init(spin_t *spin);
 void	spin_adaptive(spin_t *spin);
 #endif
 
 #if (defined(JEMALLOC_ENABLE_INLINE) || defined(JEMALLOC_SPIN_C_))
-JEMALLOC_INLINE void
-spin_init(spin_t *spin) {
-	spin->iteration = 0;
-}
-
 JEMALLOC_INLINE void
 spin_adaptive(spin_t *spin) {
 	volatile uint64_t i;
