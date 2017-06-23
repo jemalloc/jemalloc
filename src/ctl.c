d49ac4c7096b79539ce84fa1bfe122bc9e3f1b43
Jason Evans <jasone@canonware.com>
2017-06-23 10:40:02 -0700

    Fix assertion typos.
    Reported by Conrad Meyer.

--- src/ctl.c
+++ src/ctl.c
@@ -1970,7 +1970,7 @@ arena_reset_finish_background_thread(tsd_t *tsd, unsigned arena_ind) {
 			unsigned ind = arena_ind % ncpus;
 			background_thread_info_t *info =
 			    &background_thread_info[ind];
-			assert(info->state = background_thread_paused);
+			assert(info->state == background_thread_paused);
 			malloc_mutex_lock(tsd_tsdn(tsd), &info->mtx);
 			info->state = background_thread_started;
 			malloc_mutex_unlock(tsd_tsdn(tsd), &info->mtx);
