f78d4ca3fbff6cab0c704c787706a53ddafcbe13
Christopher Ferris <cferris@google.com>
2017-09-22 12:24:50 -0700

    Modify configure to determine return value of strerror_r.
    On glibc and Android's bionic, strerror_r returns char* when
    _GNU_SOURCE is defined.
    
    Add a configure check for this rather than assume glibc is the
    only libc that behaves this way.

--- src/malloc_io.c
+++ src/malloc_io.c
@@ -111,7 +111,7 @@ buferror(int err, char *buf, size_t buflen) {
 	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, err, 0,
 	    (LPSTR)buf, (DWORD)buflen, NULL);
 	return 0;
-#elif defined(__GLIBC__) && defined(_GNU_SOURCE)
+#elif defined(JEMALLOC_STRERROR_R_RETURNS_CHAR_WITH_GNU_SOURCE) && defined(_GNU_SOURCE)
 	char *b = strerror_r(err, buf, buflen);
 	if (b != buf) {
 		strncpy(buf, b, buflen);
