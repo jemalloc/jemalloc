901d94a2b06df09c960836901f6a81a0d3d00732
David T. Goldblatt <davidtgoldblatt@gmail.com>
2017-10-01 18:54:25 -0700

    Rename cache_alloc_easy to cache_bin_alloc_easy.
    This lives in the cache_bin module; just a typo.

--- include/jemalloc/internal/tcache_inlines.h
+++ include/jemalloc/internal/tcache_inlines.h
@@ -48,7 +48,7 @@ tcache_alloc_small(tsd_t *tsd, arena_t *arena, tcache_t *tcache, size_t size,
 
 	assert(binind < NBINS);
 	bin = tcache_small_bin_get(tcache, binind);
-	ret = cache_alloc_easy(bin, &tcache_success);
+	ret = cache_bin_alloc_easy(bin, &tcache_success);
 	assert(tcache_success == (ret != NULL));
 	if (unlikely(!tcache_success)) {
 		bool tcache_hard_success;
@@ -109,7 +109,7 @@ tcache_alloc_large(tsd_t *tsd, arena_t *arena, tcache_t *tcache, size_t size,
 
 	assert(binind >= NBINS &&binind < nhbins);
 	bin = tcache_large_bin_get(tcache, binind);
-	ret = cache_alloc_easy(bin, &tcache_success);
+	ret = cache_bin_alloc_easy(bin, &tcache_success);
 	assert(tcache_success == (ret != NULL));
 	if (unlikely(!tcache_success)) {
 		/*
