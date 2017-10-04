a380178e39f82bc820c910b49bec18b7fd441b2f
David Goldblatt <davidgoldblatt@fb.com>
2017-10-03 18:03:02 -0700

    Power: disable the CPU_SPINWAIT macro.
    Quoting from https://github.com/jemalloc/jemalloc/issues/761 :
    
    [...] reading the Power ISA documentation[1], the assembly in [the CPU_SPINWAIT
    macro] isn't correct anyway (as @marxin points out): the setting of the
    program-priority register is "sticky", and we never undo the lowering.
    
    We could do something similar, but given that we don't have testing here in the
    first place, I'm inclined to simply not try. I'll put something up reverting the
    problematic commit tomorrow.
    
    [1] Book II, chapter 3 of the 2.07B or 3.0B ISA documents.

--- include/jemalloc/internal/spin.h
+++ include/jemalloc/internal/spin.h
@@ -8,12 +8,22 @@ typedef struct {
 } spin_t;
 
 static inline void
+spin_cpu_spinwait() {
+#  if HAVE_CPU_SPINWAIT
+	CPU_SPINWAIT;
+#  else
+	volatile int x = 0;
+	x = x;
+#  endif
+}
+
+static inline void
 spin_adaptive(spin_t *spin) {
 	volatile uint32_t i;
 
 	if (spin->iteration < 5) {
 		for (i = 0; i < (1U << spin->iteration); i++) {
-			CPU_SPINWAIT;
+			spin_cpu_spinwait();
 		}
 		spin->iteration++;
 	} else {
