6712cd5856c2cf39008dfaf03c34170727316893
Rajeev Misra <17777442+rkmisra@users.noreply.github.com>
2018-08-03 00:10:28 -0700

    checking presence of __builtin_clz
    Fallback to older XOR way of doing thing for windows and other platforms where gcc builtin is not available.

--- include/jemalloc/internal/bit_util.h
+++ include/jemalloc/internal/bit_util.h
@@ -63,6 +63,7 @@ ffs_u32(uint32_t bitmap) {
 
 BIT_UTIL_INLINE uint64_t
 pow2_ceil_u64(uint64_t x) {
+#if (defined(__amd64__) || defined(__x86_64__) || defined(JEMALLOC_HAVE_BUILTIN_CLZ))
 	if(unlikely(x <= 1)) {
 		return x;
 	}
@@ -72,15 +73,27 @@ pow2_ceil_u64(uint64_t x) {
 			: "=r"(msb_on_index) // Outputs.
 			: "r"(x-1)           // Inputs.
 		);
-#else
+#elif (defined(JEMALLOC_HAVE_BUILTIN_CLZ))
 	msb_on_index = (63 ^ __builtin_clzll(x - 1));
 #endif
 	assert(msb_on_index < 63);
 	return 1ULL << (msb_on_index + 1);
+#else
+	x--;
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	x |= x >> 32;
+	x++;
+	return x;
+#endif
 }
 
 BIT_UTIL_INLINE uint32_t
 pow2_ceil_u32(uint32_t x) {
+#if (defined(__i386__) || defined(JEMALLOC_HAVE_BUILTIN_CLZ))
 	if(unlikely(x <= 1)) {
 		return x;
 	}
@@ -90,11 +103,21 @@ pow2_ceil_u32(uint32_t x) {
 			: "=r"(msb_on_index) // Outputs.
 			: "r"(x-1)           // Inputs.
 		);
-#else
+#elif (defined(JEMALLOC_HAVE_BUILTIN_CLZ))
 	msb_on_index = (31 ^ __builtin_clz(x - 1));
 #endif
 	assert(msb_on_index < 31);
 	return 1U << (msb_on_index + 1);
+#else
+	x--;
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	x++;
+	return x;
+#endif
 }
 
 /* Compute the smallest power of 2 that is >= x. */
