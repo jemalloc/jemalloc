04dde50102b9ceddad604935fe6fc60ced5d801b
Doron Roberts-Kedes <doronrk@fb.com>
2019-04-12 10:50:07 -0400

    meshing: keep track of mesh shapes

--- include/jemalloc/internal/mesh.h
+++ include/jemalloc/internal/mesh.h
@@ -1,5 +1,31 @@
 #ifndef JEMALLOC_INTERNAL_MESH_H
 #define JEMALLOC_INTERNAL_MESH_H
 
+#include "jemalloc/internal/mesh_stats.h"
+
 extern bool opt_mesh;
-#endif /* JEMALLOC_INTERNAL_H */
+
+typedef struct mesh_bin_data_s mesh_bin_data_t;
+struct mesh_bin_data_s {
+	mesh_bin_stats_t	stats;
+};
+
+typedef struct mesh_bin_datas_s mesh_bin_datas_t;
+struct mesh_bin_datas_s {
+	mesh_bin_data_t *bin_data_shards;
+};
+
+typedef struct mesh_arena_data_s mesh_arena_data_t;
+struct mesh_arena_data_s {
+	mesh_bin_datas_t *bin_datas;
+};
+
+bool mesh_slab_is_candidate(extent_t *slab);
+void mesh_slab_shape_add(mesh_arena_data_t *data, arena_slab_data_t *slab_data,
+    const bin_info_t *bin_info, extent_t *slab);
+void mesh_slab_shape_remove(mesh_arena_data_t *data,
+    arena_slab_data_t *slab_data, const bin_info_t *bin_info, extent_t *slab);
+mesh_arena_data_t * mesh_arena_data_new(tsdn_t *tsdn, base_t *base);
+bool mesh_boot(void);
+
+#endif /* JEMALLOC_INTERNAL_MESH_H */
--- src/arena.c
+++ src/arena.c
@@ -62,7 +62,7 @@ static bool arena_decay_dirty(tsdn_t *tsdn, arena_t *arena,
 static void arena_dalloc_bin_slab(tsdn_t *tsdn, arena_t *arena, extent_t *slab,
     bin_t *bin);
 static void arena_bin_lower_slab(tsdn_t *tsdn, arena_t *arena, extent_t *slab,
-    bin_t *bin);
+    bin_t *bin, const bin_info_t *bin_info);
 
 /******************************************************************************/
 
@@ -999,9 +999,15 @@ arena_slab_dalloc(tsdn_t *tsdn, arena_t *arena, extent_t *slab) {
 }
 
 static void
-arena_bin_slabs_nonfull_insert(bin_t *bin, extent_t *slab) {
+arena_bin_slabs_nonfull_insert(arena_t *arena, bin_t *bin,
+    const bin_info_t *bin_info, extent_t *slab) {
 	assert(extent_nfree_get(slab) > 0);
 	extent_heap_insert(&bin->slabs_nonfull, slab);
+	if (opt_mesh && mesh_slab_is_candidate(slab)) {
+		arena_slab_data_t *slab_data = extent_slab_data_get(slab);
+		mesh_slab_shape_add(arena->mesh_arena_data, slab_data, bin_info,
+		    slab);
+	}
 }
 
 static void
@@ -1010,10 +1016,14 @@ arena_bin_slabs_nonfull_remove(bin_t *bin, extent_t *slab) {
 }
 
 static extent_t *
-arena_bin_slabs_nonfull_tryget(bin_t *bin) {
+arena_bin_slabs_nonfull_tryget(arena_t *arena, bin_t *bin, const bin_info_t *bin_info) {
 	extent_t *slab = extent_heap_remove_first(&bin->slabs_nonfull);
 	if (slab == NULL) {
 		return NULL;
+	}
+	if (opt_mesh && mesh_slab_is_candidate(slab)) {
+		arena_slab_data_t *slab_data = extent_slab_data_get(slab);
+		mesh_slab_shape_remove(arena->mesh_arena_data, slab_data, bin_info, slab);
 	}
 	if (config_stats) {
 		bin->stats.reslabs++;
@@ -1248,16 +1258,14 @@ static extent_t *
 arena_bin_nonfull_slab_get(tsdn_t *tsdn, arena_t *arena, bin_t *bin,
     szind_t binind, unsigned binshard) {
 	extent_t *slab;
-	const bin_info_t *bin_info;
+	const bin_info_t *bin_info = &bin_infos[binind];
 
 	/* Look for a usable slab. */
-	slab = arena_bin_slabs_nonfull_tryget(bin);
+	slab = arena_bin_slabs_nonfull_tryget(arena, bin, bin_info);
 	if (slab != NULL) {
 		return slab;
 	}
 	/* No existing slabs have any space available. */
-
-	bin_info = &bin_infos[binind];
 
 	/* Allocate a new slab. */
 	malloc_mutex_unlock(tsdn, &bin->lock);
@@ -1278,7 +1286,7 @@ arena_bin_nonfull_slab_get(tsdn_t *tsdn, arena_t *arena, bin_t *bin,
 	 * sufficient memory available while this one dropped bin->lock above,
 	 * so search one more time.
 	 */
-	slab = arena_bin_slabs_nonfull_tryget(bin);
+	slab = arena_bin_slabs_nonfull_tryget(arena, bin, bin_info);
 	if (slab != NULL) {
 		return slab;
 	}
@@ -1322,7 +1330,7 @@ arena_bin_malloc_hard(tsdn_t *tsdn, arena_t *arena, bin_t *bin,
 					    bin);
 				} else {
 					arena_bin_lower_slab(tsdn, arena, slab,
-					    bin);
+					    bin, bin_info);
 				}
 			}
 			return ret;
@@ -1639,7 +1647,7 @@ arena_dalloc_bin_slab(tsdn_t *tsdn, arena_t *arena, extent_t *slab,
 
 static void
 arena_bin_lower_slab(tsdn_t *tsdn, arena_t *arena, extent_t *slab,
-    bin_t *bin) {
+    bin_t *bin, const bin_info_t *bin_info) {
 	assert(extent_nfree_get(slab) > 0);
 
 	/*
@@ -1651,7 +1659,8 @@ arena_bin_lower_slab(tsdn_t *tsdn, arena_t *arena, extent_t *slab,
 	if (bin->slabcur != NULL && extent_snad_comp(bin->slabcur, slab) > 0) {
 		/* Switch slabcur. */
 		if (extent_nfree_get(bin->slabcur) > 0) {
-			arena_bin_slabs_nonfull_insert(bin, bin->slabcur);
+			arena_bin_slabs_nonfull_insert(arena, bin, bin_info,
+			    bin->slabcur);
 		} else {
 			arena_bin_slabs_full_insert(arena, bin, bin->slabcur);
 		}
@@ -1660,7 +1669,7 @@ arena_bin_lower_slab(tsdn_t *tsdn, arena_t *arena, extent_t *slab,
 			bin->stats.reslabs++;
 		}
 	} else {
-		arena_bin_slabs_nonfull_insert(bin, slab);
+		arena_bin_slabs_nonfull_insert(arena, bin, bin_info, slab);
 	}
 }
 
@@ -1674,6 +1683,12 @@ arena_dalloc_bin_locked_impl(tsdn_t *tsdn, arena_t *arena, bin_t *bin,
 		arena_dalloc_junk_small(ptr, bin_info);
 	}
 
+	// If slab is not full, then its mesh shape is tracked and needs to be invalidated
+	if (opt_mesh && slab != bin->slabcur && mesh_slab_is_candidate(slab) &&
+	    !bitmap_full(slab_data->bitmap, &bin_info->bitmap_info)) {
+		mesh_slab_shape_remove(arena->mesh_arena_data, slab_data, bin_info, slab);
+	}
+
 	arena_slab_reg_dalloc(slab, slab_data, ptr);
 	unsigned nfree = extent_nfree_get(slab);
 	if (nfree == bin_info->nregs) {
@@ -1681,7 +1696,12 @@ arena_dalloc_bin_locked_impl(tsdn_t *tsdn, arena_t *arena, bin_t *bin,
 		arena_dalloc_bin_slab(tsdn, arena, slab, bin);
 	} else if (nfree == 1 && slab != bin->slabcur) {
 		arena_bin_slabs_full_remove(arena, bin, slab);
-		arena_bin_lower_slab(tsdn, arena, slab, bin);
+		arena_bin_lower_slab(tsdn, arena, slab, bin, bin_info);
+	} else if (opt_mesh && slab != bin->slabcur &&
+		   mesh_slab_is_candidate(slab) && nfree != bin_info->nregs) {
+		// If slab is not empty, then track its mesh shape
+		mesh_slab_shape_add(arena->mesh_arena_data, slab_data,
+		    bin_info, slab);
 	}
 
 	if (config_stats) {
@@ -2059,6 +2079,13 @@ arena_new(tsdn_t *tsdn, unsigned ind, extent_hooks_t *extent_hooks) {
 		}
 	}
 	assert(bin_addr == (uintptr_t)arena + arena_size);
+
+	if (opt_mesh) {
+		arena->mesh_arena_data = mesh_arena_data_new(tsdn, base);
+		if (arena->mesh_arena_data == NULL) {
+			goto label_error;
+		}
+	}
 
 	arena->base = base;
 	/* Set arena before creating background threads. */
