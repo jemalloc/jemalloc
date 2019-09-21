7cb1a677a8c292971a14843fa655cc267b0205af
David T. Goldblatt <davidtgoldblatt@gmail.com>
2019-09-21 09:36:22 -0700

    Extent -> Eset: Move extent fit functions.

--- src/eset.c
+++ src/eset.c
@@ -2,6 +2,8 @@
 #include "jemalloc/internal/jemalloc_internal_includes.h"
 
 #include "jemalloc/internal/eset.h"
+/* For opt_retain */
+#include "jemalloc/internal/extent_mmap.h"
 
 const bitmap_info_t eset_bitmap_info =
     BITMAP_INFO_INITIALIZER(SC_NPSIZES+1);
@@ -120,4 +122,114 @@ eset_remove_locked(tsdn_t *tsdn, eset_t *eset, extent_t *extent) {
 	assert(cur_extents_npages >= npages);
 	atomic_store_zu(&eset->npages,
 	    cur_extents_npages - (size >> LG_PAGE), ATOMIC_RELAXED);
+}
+
+/*
+ * Find an extent with size [min_size, max_size) to satisfy the alignment
+ * requirement.  For each size, try only the first extent in the heap.
+ */
+static extent_t *
+eset_fit_alignment(eset_t *eset, size_t min_size, size_t max_size,
+    size_t alignment) {
+        pszind_t pind = sz_psz2ind(sz_psz_quantize_ceil(min_size));
+        pszind_t pind_max = sz_psz2ind(sz_psz_quantize_ceil(max_size));
+
+	for (pszind_t i = (pszind_t)bitmap_ffu(eset->bitmap,
+	    &eset_bitmap_info, (size_t)pind); i < pind_max; i =
+	    (pszind_t)bitmap_ffu(eset->bitmap, &eset_bitmap_info,
+	    (size_t)i+1)) {
+		assert(i < SC_NPSIZES);
+		assert(!extent_heap_empty(&eset->heaps[i]));
+		extent_t *extent = extent_heap_first(&eset->heaps[i]);
+		uintptr_t base = (uintptr_t)extent_base_get(extent);
+		size_t candidate_size = extent_size_get(extent);
+		assert(candidate_size >= min_size);
+
+		uintptr_t next_align = ALIGNMENT_CEILING((uintptr_t)base,
+		    PAGE_CEILING(alignment));
+		if (base > next_align || base + candidate_size <= next_align) {
+			/* Overflow or not crossing the next alignment. */
+			continue;
+		}
+
+		size_t leadsize = next_align - base;
+		if (candidate_size - leadsize >= min_size) {
+			return extent;
+		}
+	}
+
+	return NULL;
+}
+
+/*
+ * Do first-fit extent selection, i.e. select the oldest/lowest extent that is
+ * large enough.
+ */
+static extent_t *
+eset_first_fit_locked(tsdn_t *tsdn, eset_t *eset, size_t size) {
+	extent_t *ret = NULL;
+
+	pszind_t pind = sz_psz2ind(sz_psz_quantize_ceil(size));
+
+	if (!maps_coalesce && !opt_retain) {
+		/*
+		 * No split / merge allowed (Windows w/o retain). Try exact fit
+		 * only.
+		 */
+		return extent_heap_empty(&eset->heaps[pind]) ? NULL :
+		    extent_heap_first(&eset->heaps[pind]);
+	}
+
+	for (pszind_t i = (pszind_t)bitmap_ffu(eset->bitmap,
+	    &eset_bitmap_info, (size_t)pind);
+	    i < SC_NPSIZES + 1;
+	    i = (pszind_t)bitmap_ffu(eset->bitmap, &eset_bitmap_info,
+	    (size_t)i+1)) {
+		assert(!extent_heap_empty(&eset->heaps[i]));
+		extent_t *extent = extent_heap_first(&eset->heaps[i]);
+		assert(extent_size_get(extent) >= size);
+		/*
+		 * In order to reduce fragmentation, avoid reusing and splitting
+		 * large eset for much smaller sizes.
+		 *
+		 * Only do check for dirty eset (delay_coalesce).
+		 */
+		if (eset->delay_coalesce &&
+		    (sz_pind2sz(i) >> opt_lg_extent_max_active_fit) > size) {
+			break;
+		}
+		if (ret == NULL || extent_snad_comp(extent, ret) < 0) {
+			ret = extent;
+		}
+		if (i == SC_NPSIZES) {
+			break;
+		}
+		assert(i < SC_NPSIZES);
+	}
+
+	return ret;
+}
+
+extent_t *
+eset_fit_locked(tsdn_t *tsdn, eset_t *eset, size_t esize, size_t alignment) {
+	malloc_mutex_assert_owner(tsdn, &eset->mtx);
+
+	size_t max_size = esize + PAGE_CEILING(alignment) - PAGE;
+	/* Beware size_t wrap-around. */
+	if (max_size < esize) {
+		return NULL;
+	}
+
+	extent_t *extent = eset_first_fit_locked(tsdn, eset, max_size);
+
+	if (alignment > PAGE && extent == NULL) {
+		/*
+		 * max_size guarantees the alignment requirement but is rather
+		 * pessimistic.  Next we try to satisfy the aligned allocation
+		 * with sizes in [esize, max_size).
+		 */
+		extent = eset_fit_alignment(eset, esize, max_size, alignment);
+	}
+
+	return extent;
 }
