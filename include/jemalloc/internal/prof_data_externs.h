ea42174d07c2cf496e407bfae74be866ee090b2f
Yinan Zhang <zyn8950@gmail.com>
2019-12-18 15:15:31 -0800

    Refactor profiling headers

---
+++ include/jemalloc/internal/prof_data_externs.h
@@ -0,0 +1,22 @@
+#ifndef JEMALLOC_INTERNAL_PROF_DATA_EXTERNS_H
+#define JEMALLOC_INTERNAL_PROF_DATA_EXTERNS_H
+
+#include "jemalloc/internal/mutex.h"
+
+extern malloc_mutex_t *gctx_locks;
+extern malloc_mutex_t *tdata_locks;
+
+void prof_bt_hash(const void *key, size_t r_hash[2]);
+bool prof_bt_keycomp(const void *k1, const void *k2);
+
+bool prof_data_init(tsd_t *tsd);
+bool prof_dump(tsd_t *tsd, bool propagate_err, const char *filename,
+    bool leakcheck);
+prof_tdata_t * prof_tdata_init_impl(tsd_t *tsd, uint64_t thr_uid,
+    uint64_t thr_discrim, char *thread_name, bool active, bool reset_interval);
+void prof_tdata_detach(tsd_t *tsd, prof_tdata_t *tdata);
+void bt_init(prof_bt_t *bt, void **vec);
+void prof_backtrace(tsd_t *tsd, prof_bt_t *bt);
+void prof_tctx_try_destroy(tsd_t *tsd, prof_tctx_t *tctx);
+
+#endif /* JEMALLOC_INTERNAL_PROF_DATA_EXTERNS_H */
---
+++ include/jemalloc/internal/prof_log_externs.h
@@ -0,0 +1,17 @@
+#ifndef JEMALLOC_INTERNAL_PROF_LOG_EXTERNS_H
+#define JEMALLOC_INTERNAL_PROF_LOG_EXTERNS_H
+
+#include "jemalloc/internal/mutex.h"
+
+void prof_try_log(tsd_t *tsd, size_t usize, prof_info_t *prof_info);
+bool prof_log_init(tsd_t *tsdn);
+#ifdef JEMALLOC_JET
+size_t prof_log_bt_count(void);
+size_t prof_log_alloc_count(void);
+size_t prof_log_thr_count(void);
+bool prof_log_is_logging(void);
+bool prof_log_rep_check(void);
+void prof_log_dummy_set(bool new_value);
+#endif
+
+#endif /* JEMALLOC_INTERNAL_PROF_LOG_EXTERNS_H */
