9d78b95ce40a48b38a1451db2ac71b45af38fc54
David Goldblatt <davidgoldblatt@fb.com>
2020-03-07 20:14:49 -0800

    Add a stub PA module -- a page allocator.

---
+++ include/jemalloc/internal/pa.h
@@ -0,0 +1,9 @@
+#ifndef JEMALLOC_INTERNAL_PA_H
+#define JEMALLOC_INTERNAL_PA_H
+
+/*
+ * The page allocator; responsible for acquiring pages of memory for
+ * allocations.
+ */
+
+#endif /* JEMALLOC_INTERNAL_PA_H */
---
+++ src/pa.c
@@ -0,0 +1,2 @@
+#include "jemalloc/internal/jemalloc_preamble.h"
+#include "jemalloc/internal/jemalloc_internal_includes.h"
