3192d6b77dae3b4aa36b95eea793fcdea6f5ffbd
David Goldblatt <davidgoldblatt@fb.com>
2020-03-09 12:20:06 -0700

    Extents: Have extent_dalloc_gap take ehooks.
    We're almost to the point where the extent code doesn't know about arenas at
    all.  In that world, we shouldn't pull them out of the arena.

--- include/jemalloc/internal/extent.h
+++ include/jemalloc/internal/extent.h
@@ -33,7 +33,8 @@ edata_t *ecache_evict(tsdn_t *tsdn, arena_t *arena, ehooks_t *ehooks,
 edata_t *extent_alloc_wrapper(tsdn_t *tsdn, arena_t *arena, ehooks_t *ehooks,
     void *new_addr, size_t size, size_t alignment, bool slab, szind_t szind,
     bool *zero, bool *commit);
-void extent_dalloc_gap(tsdn_t *tsdn, arena_t *arena, edata_t *edata);
+void extent_dalloc_gap(tsdn_t *tsdn, arena_t *arena, ehooks_t *ehooks,
+    edata_t *edata);
 void extent_dalloc_wrapper(tsdn_t *tsdn, arena_t *arena, ehooks_t *ehooks,
     edata_t *edata);
 void extent_destroy_wrapper(tsdn_t *tsdn, arena_t *arena, ehooks_t *ehooks,
