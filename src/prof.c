adfd9d7b1d69a997a74193bf9d03951616f22ba6
Yinan Zhang <zyn8950@gmail.com>
2020-03-31 10:43:04 -0700

    Change tsdn to tsd for thread name allocation

--- src/prof.c
+++ src/prof.c
@@ -746,7 +746,7 @@ prof_tdata_reinit(tsd_t *tsd, prof_tdata_t *tdata) {
 	uint64_t thr_uid = tdata->thr_uid;
 	uint64_t thr_discrim = tdata->thr_discrim + 1;
 	char *thread_name = (tdata->thread_name != NULL) ?
-	    prof_thread_name_alloc(tsd_tsdn(tsd), tdata->thread_name) : NULL;
+	    prof_thread_name_alloc(tsd, tdata->thread_name) : NULL;
 	bool active = tdata->active;
 
 	prof_tdata_detach(tsd, tdata);
