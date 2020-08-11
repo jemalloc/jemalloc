5e90fd006e97d62d74c79ce67cbf0cae5429ecdc
David Goldblatt <davidgoldblatt@fb.com>
2020-08-11 10:18:31 -0700

    Geom_grow: Don't keep the mutex internal.
    We're about to use it in ways that will have external synchronization.

--- src/geom_grow.c
+++ src/geom_grow.c
@@ -1,29 +1,8 @@
 #include "jemalloc/internal/jemalloc_preamble.h"
 #include "jemalloc/internal/jemalloc_internal_includes.h"
 
-bool
+void
 geom_grow_init(geom_grow_t *geom_grow) {
 	geom_grow->next = sz_psz2ind(HUGEPAGE);
 	geom_grow->limit = sz_psz2ind(SC_LARGE_MAXCLASS);
-	if (malloc_mutex_init(&geom_grow->mtx, "extent_grow",
-	    WITNESS_RANK_EXTENT_GROW, malloc_mutex_rank_exclusive)) {
-		return true;
-	}
-	return false;
 }
-
-void
-geom_grow_prefork(tsdn_t *tsdn, geom_grow_t *geom_grow) {
-	malloc_mutex_prefork(tsdn, &geom_grow->mtx);
-}
-
-void
-geom_grow_postfork_parent(tsdn_t *tsdn, geom_grow_t *geom_grow) {
-	malloc_mutex_postfork_parent(tsdn, &geom_grow->mtx);
-}
-
-void
-geom_grow_postfork_child(tsdn_t *tsdn, geom_grow_t *geom_grow) {
-	malloc_mutex_postfork_child(tsdn, &geom_grow->mtx);
-}
-
