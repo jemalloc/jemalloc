634ec6f50abd57e6371e0c745ab699f2cf6d08e6
David Goldblatt <davidgoldblatt@fb.com>
2020-09-18 15:50:27 -0700

    Edata: add an "age" field.

--- src/edata.c
+++ src/edata.c
@@ -4,3 +4,4 @@
 ph_gen(, edata_avail_, edata_tree_t, edata_t, ph_link,
     edata_esnead_comp)
 ph_gen(, edata_heap_, edata_heap_t, edata_t, ph_link, edata_snad_comp)
+ph_gen(, edata_age_heap_, edata_age_heap_t, edata_t, ph_link, edata_age_comp)
