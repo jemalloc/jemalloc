06b55a197d421d70acd4fa8d34fa23ce88997cec
David Goldblatt <davidgoldblatt@fb.com>
2020-11-04 16:00:52 -0800

    Rename edata_tree_t -> edata_avail_t.
    This isn't a tree any more, and it mildly irritates me any time I see it.

--- src/edata.c
+++ src/edata.c
@@ -1,7 +1,7 @@
 #include "jemalloc/internal/jemalloc_preamble.h"
 #include "jemalloc/internal/jemalloc_internal_includes.h"
 
-ph_gen(, edata_avail_, edata_tree_t, edata_t, ph_link,
+ph_gen(, edata_avail_, edata_avail_t, edata_t, ph_link,
     edata_esnead_comp)
 ph_gen(, edata_heap_, edata_heap_t, edata_t, ph_link, edata_snad_comp)
 ph_gen(, edata_age_heap_, edata_age_heap_t, edata_t, ph_link, edata_age_comp)
