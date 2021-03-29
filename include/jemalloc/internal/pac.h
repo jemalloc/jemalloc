30e757fdde5a644a33e3d547afad2a69b4aeda3e
Evers Chen <evers_chen@163.com>
2021-03-30 07:27:37 +0800

    Remove redundant declaration, pac_retain_grow_limit_get_set was declared twice in pac.h

--- include/jemalloc/internal/pac.h
+++ include/jemalloc/internal/pac.h
@@ -121,8 +121,6 @@ bool pac_init(tsdn_t *tsdn, pac_t *pac, base_t *base, emap_t *emap,
     edata_cache_t *edata_cache, nstime_t *cur_time, size_t oversize_threshold,
     ssize_t dirty_decay_ms, ssize_t muzzy_decay_ms, pac_stats_t *pac_stats,
     malloc_mutex_t *stats_mtx);
-bool pac_retain_grow_limit_get_set(tsdn_t *tsdn, pac_t *pac, size_t *old_limit,
-    size_t *new_limit);
 void pac_stats_merge(tsdn_t *tsdn, pac_t *pac, pac_stats_t *pac_stats_out,
     pac_estats_t *estats_out, size_t *resident);
 
