a90d6be7e930279f4fc5f15ecbba36838f8a506c
Charles <a837940593@gmail.com>
2021-07-23 23:59:06 +0800

    Remove unless function prof_dump_ctl

--- src/ctl.c
+++ src/ctl.c
@@ -3175,29 +3175,6 @@ label_return:
 }
 
 static int
-prof_dump_ctl(tsd_t *tsd, const size_t *mib, size_t miblen,
-    void *oldp, size_t *oldlenp, void *newp, size_t newlen) {
-	int ret;
-	const char *filename = NULL;
-
-	if (!config_prof) {
-		return ENOENT;
-	}
-
-	WRITEONLY();
-	WRITE(filename, const char *);
-
-	if (prof_mdump(tsd, filename)) {
-		ret = EFAULT;
-		goto label_return;
-	}
-
-	ret = 0;
-label_return:
-	return ret;
-}
-
-static int
 prof_gdump_ctl(tsd_t *tsd, const size_t *mib, size_t miblen,
     void *oldp, size_t *oldlenp, void *newp, size_t newlen) {
 	int ret;
