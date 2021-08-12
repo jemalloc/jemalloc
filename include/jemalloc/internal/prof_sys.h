5884a076fb858320e7bcf86b961dd1555a81a75e
Qi Wang <interwq@gwu.edu>
2021-08-12 15:48:02 -0700

    Rename prof.dump_prefix to prof.prefix
    This better aligns with our naming convention.  The option has not been included
    in any upstream release yet.

--- include/jemalloc/internal/prof_sys.h
+++ include/jemalloc/internal/prof_sys.h
@@ -10,7 +10,7 @@ void prof_unwind_init();
 void prof_sys_thread_name_fetch(tsd_t *tsd);
 int prof_getpid(void);
 void prof_get_default_filename(tsdn_t *tsdn, char *filename, uint64_t ind);
-bool prof_dump_prefix_set(tsdn_t *tsdn, const char *prefix);
+bool prof_prefix_set(tsdn_t *tsdn, const char *prefix);
 void prof_fdump_impl(tsd_t *tsd);
 void prof_idump_impl(tsd_t *tsd);
 bool prof_mdump_impl(tsd_t *tsd, const char *filename);
