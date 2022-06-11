adc70c051135ac8909ca37492d7b104150077033
David CARLIER <devnexen@gmail.com>
2022-06-11 07:57:22 +0100

    update travis

--- scripts/gen_travis.py
+++ scripts/gen_travis.py
@@ -257,7 +257,6 @@ def generate_macos(arch):
 
     exclude = ([Option.as_malloc_conf(opt) for opt in (
             'dss:primary',
-            'percpu_arena:percpu',
             'background_thread:true')] +
         [Option.as_configure_flag('--enable-prof')] +
         [CLANG,])
