5d5f76ee015696e0e086650e85722ceca9d191c1
Slobodan Predolac <spredolac@fb.com>
2025-08-26 15:15:08 -0700

    Remove pidfd_open call handling and rely on PIDFD_SELF

--- include/jemalloc/internal/pages.h
+++ include/jemalloc/internal/pages.h
@@ -127,6 +127,5 @@ bool pages_boot(void);
 void pages_set_thp_state(void *ptr, size_t size);
 void pages_mark_guards(void *head, void *tail);
 void pages_unmark_guards(void *head, void *tail);
-void pages_postfork_child(void);
 
 #endif /* JEMALLOC_INTERNAL_PAGES_EXTERNS_H */
