2688047b56e6ef21d960e40281cb13774c8c17ab
Slobodan Predolac <spredolac@fb.com>
2025-09-29 09:41:51 -0700

    Revert "Do not dehugify when purging"
    This reverts commit 16c5abd1cd0a21e8f985f77d8e342c8ed91450d7.

--- include/jemalloc/internal/hpa_hooks.h
+++ include/jemalloc/internal/hpa_hooks.h
@@ -10,6 +10,7 @@ struct hpa_hooks_s {
 	void (*unmap)(void *ptr, size_t size);
 	void (*purge)(void *ptr, size_t size);
 	bool (*hugify)(void *ptr, size_t size, bool sync);
+	void (*dehugify)(void *ptr, size_t size);
 	void (*curtime)(nstime_t *r_time, bool first_reading);
 	uint64_t (*ms_since)(nstime_t *r_time);
 	bool (*vectorized_purge)(void *vec, size_t vlen, size_t nbytes);
